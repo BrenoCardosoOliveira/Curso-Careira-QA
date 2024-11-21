0d9c68a83dbb339b48b6180c17d9901742684fd8
Breno Cardoso de Oliveira <97921444+BrenoCardosoOliveira@users.noreply.github.com>
2024-11-20 21:12:26 -0300

    Add files via upload

---
+++ Unidade 1 - Carreira QA Processo e Automação de Testes/Módulo 2 Adquirindo a base para começar a automatizar testes/Etapa 1 Lógica de Programação com JavaScript/Desavio 4/logica-js-projeto_desafio4_12/app.js
@@ -0,0 +1,3 @@
+// 12. Use o Math.ramdon para gerar um número inteiro entre 1 e 10 e exiba esse número no console.  
+let numeroInteiroAleatorio = parseInt(Math.random() * 10) + 1;
+console.log(numeroInteiroAleatorio);
---
+++ Unidade 1 - Carreira QA Processo e Automação de Testes/Módulo 2 Adquirindo a base para começar a automatizar testes/Etapa 1 Lógica de Programação com JavaScript/Desavio 4/logica-js-projeto_desafio4_4/app.js
@@ -0,0 +1,3 @@
+// 4. Utilize o prompt e faça a seguinte pergunta: Qual  a linguagem de programação que você mais gosta?. Em seguida, armazene a resposta em uma variável e mostre no console do navegador.
+let linguagemPreferida = prompt('Qual  a linguagem de programação que você mais gosta?');
+console.log(linguagemPreferida);
